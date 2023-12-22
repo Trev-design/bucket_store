f8f5ff4964cda6aabf6ddcc6c7631789acddf7c0
Tev-design <gerrit.flick@gmail.com>
2023-12-22 22:16:20 +0100

    making tests and fixing code

--- apps/bucket/lib/bucket/access.ex
+++ apps/bucket/lib/bucket/access.ex
@@ -5,35 +5,43 @@ defmodule Bucket.Access do
   def init(:ok), do: {:ok, %{}}
 
   def get(bucket, key), do: GenServer.call(bucket, {:get, key})
-  def show_bucket(bucket), GenServer.call(bucket, :show)
+  def show_bucket(bucket), do: GenServer.call(bucket, :show)
   def put(bucket, key, value), do: GenServer.call(bucket, {:set, key, value})
-  def delete_items(bucket, key), GenServer.call(bucket, {:delete, key})
-  def reduce_count(bucket, key, count), GenServer.call(bucket, {:reduce, key, count})
-  def finish(bucket), GenServer.call(bucket, :terminate)
+  def delete_items(bucket, key), do: GenServer.call(bucket, {:delete, key})
+  def reduce_count(bucket, key, count), do: GenServer.call(bucket, {:reduce, key, count})
+  def finish(bucket), do: GenServer.call(bucket, :terminate)
 
   def handle_call({:get, key}, _from, state) do
     case Map.fetch(state, key) do
       {:ok, value} -> {:reply, {:ok, value}, state}
-      :error       -> {:reply, {:error, "no item with this name"}}
+      :error       -> {:reply, {:error, "no item with this name"}, state}
     end
   end
 
   def handle_call(:show, _from, state), do: {:reply, state, state}
 
-  def handle_call({:set, key, value}, _from, state) do
+  def handle_call({:set, key, value}, _from, state) when is_integer(value) do
     case Map.fetch(state, key) do
       {:ok, current} -> {:reply, :ok, Map.put(state, key, current + value)}
       :error         -> {:reply, :ok, Map.put(state, key,value)}
     end
   end
 
+  def handle_call({:set, _key, _value}, _from, state) do
+    {:reply, {:error, "value not an integer"}, state}
+  end
+
   def handle_call({:delete, key}, _from, state), do: {:reply, :ok, Map.delete(state, key)}
 
-  def handle_call({:reduce, key, count}, _from, state) do
+  def handle_call({:reduce, key, count}, _from, state) when is_integer(count) do
     case Map.fetch(state, key) do
       {:ok, current} -> reduce(state, key, current, count)
       :error         -> {:reply, {:error, "no item with this name in this bucket"}, state}
     end
+  end
+
+  def handle_call({:reduce, _key, _count}, _from, state) do
+    {:reply, {:error, "count not an integer"}, state}
   end
 
   def handle_call(:terminate, _from, state) do
---
+++ apps/bucket/test/bucket_access_test.exs
@@ -0,0 +1,45 @@
+defmodule Bucket.Test.BucketAccessTest do
+  use ExUnit.Case, async: true
+
+  setup do
+    {:ok, bucket} = Bucket.Access.start_link([])
+    %{bucket: bucket}
+  end
+
+  test "try to put more data in", %{bucket: bucket} do
+    assert Bucket.Access.put(bucket, "milk", "2") == {:error, "value not an integer"}
+    assert Bucket.Access.put(bucket, "milk", 2) == :ok
+  end
+
+  test "looking for the whole bucket", %{bucket: bucket} do
+    assert Bucket.Access.show_bucket(bucket) == %{}
+    Bucket.Access.put(bucket, "milk", 3)
+    Bucket.Access.put(bucket, "butter", 2)
+    assert Bucket.Access.show_bucket(bucket) == %{"milk" => 3, "butter" => 2}
+  end
+
+  test "try to make a lookup in bucket", %{bucket: bucket} do
+    assert Bucket.Access.get(bucket, "milk") == {:error, "no item with this name"}
+    Bucket.Access.put(bucket, "milk", 3)
+    assert Bucket.Access.get(bucket, "milk") == {:ok, 3}
+  end
+
+  test "try to delete an item", %{bucket: bucket} do
+    Bucket.Access.put(bucket, "milk", 3)
+    assert Bucket.Access.get(bucket, "milk") == {:ok, 3}
+    Bucket.Access.delete_items(bucket, "milk")
+    assert Bucket.Access.get(bucket, "milk") == {:error, "no item with this name"}
+  end
+
+  test "try to reduce item count", %{bucket: bucket} do
+    Bucket.Access.put(bucket, "milk", 3)
+    assert Bucket.Access.get(bucket, "milk") == {:ok, 3}
+    assert Bucket.Access.reduce_count(bucket, "butter", 5) == {:error, "no item with this name in this bucket"}
+    assert Bucket.Access.reduce_count(bucket, "milk", "4") == {:error, "count not an integer"}
+    assert Bucket.Access.reduce_count(bucket, "milk", 4) == {:error, "you don't have enough items in this bucket"}
+    Bucket.Access.reduce_count(bucket, "milk", 2)
+    assert Bucket.Access.get(bucket, "milk") == {:ok, 1}
+    Bucket.Access.reduce_count(bucket, "milk", 1)
+    assert Bucket.Access.get(bucket, "milk") == {:error, "no item with this name"}
+  end
+end
