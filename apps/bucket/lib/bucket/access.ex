e2c34104b2b7cd87912e14bb92b3fdd63371b45e
Tev-design <gerrit.flick@gmail.com>
2023-12-23 20:51:51 +0100

    tipehints and implementation declarations

--- apps/bucket/lib/bucket/access.ex
+++ apps/bucket/lib/bucket/access.ex
@@ -2,15 +2,31 @@ defmodule Bucket.Access do
   use GenServer, restart: :temporary
 
   def start_link(opts), do: GenServer.start_link(__MODULE__, :ok, opts)
+
+  @impl GenServer
   def init(:ok), do: {:ok, %{}}
 
+  @spec get(bucket :: pid(), key :: binary()) ::
+    {:ok, num_items :: non_neg_integer()} |
+    {:error, reason :: binary()}
   def get(bucket, key), do: GenServer.call(bucket, {:get, key})
+
+  @spec show_bucket(bucket :: pid()) :: items :: map()
   def show_bucket(bucket), do: GenServer.call(bucket, :show)
+
+  @spec put(bucket :: pid(), key :: binary(), value :: non_neg_integer()) ::
+    :ok | {:error, reason :: binary()}
   def put(bucket, key, value), do: GenServer.call(bucket, {:set, key, value})
+
+  @spec delete_items(bucket :: pid(), key :: binary()) :: :ok
   def delete_items(bucket, key), do: GenServer.call(bucket, {:delete, key})
+
+  @spec reduce_count(bucket :: pid(), key :: binary(), count :: non_neg_integer()) ::
+    :ok | {:error, reason :: binary()}
   def reduce_count(bucket, key, count), do: GenServer.call(bucket, {:reduce, key, count})
   def finish(bucket), do: GenServer.call(bucket, :terminate)
 
+  @impl GenServer
   def handle_call({:get, key}, _from, state) do
     case Map.fetch(state, key) do
       {:ok, value} -> {:reply, {:ok, value}, state}
@@ -18,8 +34,10 @@ defmodule Bucket.Access do
     end
   end
 
+  @impl GenServer
   def handle_call(:show, _from, state), do: {:reply, state, state}
 
+  @impl GenServer
   def handle_call({:set, key, value}, _from, state) when is_integer(value) do
     case Map.fetch(state, key) do
       {:ok, current} -> {:reply, :ok, Map.put(state, key, current + value)}
@@ -27,12 +45,15 @@ defmodule Bucket.Access do
     end
   end
 
+  @impl GenServer
   def handle_call({:set, _key, _value}, _from, state) do
     {:reply, {:error, "value not an integer"}, state}
   end
 
+  @impl GenServer
   def handle_call({:delete, key}, _from, state), do: {:reply, :ok, Map.delete(state, key)}
 
+  @impl GenServer
   def handle_call({:reduce, key, count}, _from, state) when is_integer(count) do
     case Map.fetch(state, key) do
       {:ok, current} -> reduce(state, key, current, count)
@@ -40,10 +61,12 @@ defmodule Bucket.Access do
     end
   end
 
+  @impl GenServer
   def handle_call({:reduce, _key, _count}, _from, state) do
     {:reply, {:error, "count not an integer"}, state}
   end
 
+  @impl GenServer
   def handle_call(:terminate, _from, state) do
     {:stop, :normal, {:ok, state}, state}
   end
--- apps/bucket/lib/bucket/registry.ex
+++ apps/bucket/lib/bucket/registry.ex
@@ -3,15 +3,22 @@ defmodule Bucket.Registry do
 
   def start_link(opts), do: GenServer.start_link(__MODULE__, :ok, opts)
 
+  @impl GenServer
   def init(:ok) do
     :ets.new(:registries, [:named_table, read_concurrency: true])
     :ets.new(:refs, [:named_table, read_concurrency: true])
     {:ok, :ok}
   end
 
+  @spec lookup(name :: binary()) ::
+    {:ok, bucket :: pid()} |
+    {:error, reason :: binary()}
   def lookup(name), do: GenServer.call(__MODULE__, {:lookup, name})
+
+  @spec create(name :: binary()) :: :ok
   def create(name), do: GenServer.cast(__MODULE__, {:create, name})
 
+  @impl GenServer
   def handle_call({:lookup, name}, _from, state) do
     case :ets.lookup(:registries, name) do
       []                -> {:reply, {:error, "no bucket registered with this name"}, state}
@@ -19,6 +26,7 @@ defmodule Bucket.Registry do
     end
   end
 
+  @impl GenServer
   def handle_cast({:create, name}, state) do
     case :ets.member(:registries, name) do
       true  -> {:noreply, state}
@@ -31,6 +39,7 @@ defmodule Bucket.Registry do
     end
   end
 
+  @impl GenServer
   def handle_info({:DOWN, ref, :process, _pid, _reason}, state) do
     [{^ref, name}] = :ets.lookup(:refs, ref)
     :ets.delete(:registries, name)
